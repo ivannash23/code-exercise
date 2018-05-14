3e30aee04262f76d7a9cd3734248c3fcd2c48961
ivannash23 <ivannash23@Gmail.com>
2018-05-14 11:04:03 -0700

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+	twitter: {
+  	consumer_key: 'eAIYx2F9MH4cW1jXUKCJn2CN5',
+		consumer_secret: 'SK5f8IcopO1fddHE9WPj0ovS93kCuEfahVLStHnI1U24kXNVHD',
+		access_token: '994482334043795456-fh6sPtctbXhMmjnYHW4BLR0sxv96brV',
+		access_token_secret: 'A05RbvYYyQMH2goPkXPj0Fr1h4YUkdI1q0w8P5OuRmeuO'
+	}
+
+};
